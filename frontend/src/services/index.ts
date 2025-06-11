02b276d1470eaf4e7b04eeb901762825152e75ca
Thinh <heyiamthinh1003@gmail.com>
2025-06-11 15:52:29 +0700

    chore(deps): initialize package.json and package-lock.json with recharts dependency

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -11,14 +11,14 @@ export { customerService } from './customerService';
 export { demoService } from './demoService';
 export { employeeService } from './employeeService';
 export { flightDetailService } from './flightDetailService';
-export { flightService } from './flightService';
+export { flightService } from './FlightService';
 export { flightTicketClassService } from './flightTicketClassService';
 export { messageService } from './messageService';
 export { passengerService } from './passengerService';
 export { planeService } from './planeService';
 export { ticketClassService } from './ticketClassService';
 export { ticketService } from './ticketService';
-export { parameterService } from './parameterService';
+export { parameterService } from './ParameterService';
 export { paymentService } from './paymentService';
 export { debugService } from './debugService';
 
