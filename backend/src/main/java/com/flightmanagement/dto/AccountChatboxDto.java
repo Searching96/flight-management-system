31a787e849836bb3faefb202de2bf3275b599c04
OctGuy <heyiamthinh1003@gmail.com>
2025-10-07 23:08:21 +0700

    refactor: simplify DTOs by adding Lombok annotations and removing boilerplate code

--- backend/src/main/java/com/flightmanagement/dto/AccountChatboxDto.java
+++ backend/src/main/java/com/flightmanagement/dto/AccountChatboxDto.java
@@ -1,11 +1,16 @@
 package com.flightmanagement.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountChatboxDto {
+
     private Integer accountId;
     private Integer chatboxId;
     private String accountName;
